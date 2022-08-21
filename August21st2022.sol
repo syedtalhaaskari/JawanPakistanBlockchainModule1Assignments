7b47c8cb6e034d332efecd1217ba2e36cb99f635
syedtalhaaskari <syedtalhaaskari@gmail.com>
2022-08-21 16:36:15 +0500

    Task 2 complete

--- August21st2022.sol
+++ August21st2022.sol
@@ -6,3 +6,16 @@ pragma solidity ^0.8.15;
 contract Task1 {
     string public str = "Hello Solidity";
 }
+
+contract Task2 {
+    uint8 private globalVariable = 10;
+
+    function returnStateVariable() external view returns(uint8) {
+        return globalVariable;
+    }
+
+    function returnLocalVariable() external pure returns(uint8) {
+        uint8 localVariable = 20;
+        return localVariable;
+    }
+}
