37ef525c343732b0a7a770ba6672bb1bcc326f9e
syedtalhaaskari <syedtalhaaskari@gmail.com>
2022-09-03 14:04:09 +0500

    Task 1 Complete

---
+++ August29th2022.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+// Jawan Pakistan Assignment August 29th 2022
+
+contract Task1 {
+    function power(uint x, uint y) external pure returns (uint) {
+        return x ** y;
+    }
+}
