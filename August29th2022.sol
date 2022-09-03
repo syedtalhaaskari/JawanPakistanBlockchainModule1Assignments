1929021d273adddc7e702d1b6729aee5baf7412f
syedtalhaaskari <syedtalhaaskari@gmail.com>
2022-09-03 14:04:26 +0500

    Task 2 Complete

--- August29th2022.sol
+++ August29th2022.sol
@@ -8,3 +8,20 @@ contract Task1 {
         return x ** y;
     }
 }
+
+contract Task2 {
+    function palindrome(uint n) external pure returns (uint8) {
+        uint orgNum = n;
+        uint reverseNum = 0;
+        uint remainder;
+        while(orgNum > 0) {
+            remainder = orgNum % 10;
+            reverseNum = reverseNum * 10 + remainder;
+            orgNum = orgNum / 10;
+        }
+        if (reverseNum == n){
+            return 1;
+        }
+        return 0;
+    }
+}
